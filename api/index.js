e0c0de6fdb989fb2449ff889ee78bf4662481342
igorLAO <igooli670@gmail.com>
2023-08-22 10:52:38 -0300

    backend

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import cors from "cors";
+import express from 'express';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+server.listen(process.env.PORT,() => console.log(`
+        API ONLINE NA PORTA ${process.env.PORT}
+`));
+
+
+server.get('/japodemossar',async (req, resp) =>{
+    let horario = Date()
+    resp.send('pode, pois são '+ horario)
+});
