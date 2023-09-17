2fc0eb06f3c648fa9962735c26e5ddd8d624c463
igorLAO <igooli670@gmai.com>
2023-09-17 12:56:12 -0300

    tarefa 17/09 Concluida

--- api/src/repositorys/usuarios/usuariosRepo.js
+++ api/src/repositorys/usuarios/usuariosRepo.js
@@ -1,10 +1,17 @@
 import config from "../DB/db_connection.js";
 
 export async function getUsers(Tier) {
-    let sql = `	SELECT *
-                      FROM USERS_TB	        AS C_TB
-                INNER JOIN ENDERECOS_TB     AS E_TB 
-                                            ON E_TB.ID_ENDERECO= C_TB.ID_ENDERECO`;
+    let sql = `	SELECT  ID_USUARIO      AS      id,
+                        NM_USUARIO      AS      Nome,
+                        DS_EMAIL        AS      Email,
+                        DS_TELEFONE     AS      Telefone,
+                        DS_CPF          AS      CPF,
+                        NM_CIDADE       AS      Nome_Cidade,
+                        NM_RUA          AS      Nome_Rua,
+                        NR_NUMERO       AS      Numero  
+                      FROM USERS_TB	    AS C_TB
+                INNER JOIN ENDERECOS_TB AS E_TB 
+                                        ON E_TB.ID_ENDERECO= C_TB.ID_ENDERECO`;
     let [resp] = await config.query(sql)
     return resp
 };
@@ -41,8 +48,19 @@ export async function SearchUser(search){
                          FROM USERS_TB
                 WHERE NM_USUARIO  LIKE ? 
                       OR DS_CPF   LIKE ?
-                      OR DS_EMAIL LIKE?`
-    let [resp] = await config.query(sql, [`% ${search} % `,
-                                          `% ${search} % `,
-                                          `% ${search} % `])
+                      OR DS_EMAIL LIKE ?`
+    let [resp] = await config.query(sql, [  '%' + search + '%',
+                                            '%' + search + '%',
+                                            '%' + search + '%'])
+    console.log(resp)
+    
+    return resp
+}
+
+export async function Delete(id){
+    let sql = `DELETE FROM USERS_TB
+                      WHERE ID_USUARIO = ?`;
+    let [resp] = await config.query(sql, [ id])
+    
+    return resp.affectedRows
 }
