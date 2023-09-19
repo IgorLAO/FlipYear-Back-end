c44d30da2521d9af550981455aefa525b68feed1
igorLAO <igooli670@gmail.com>
2023-09-19 11:56:04 -0300

    OK

--- database/DML.sql
+++ database/DML.sql
@@ -1,15 +1,20 @@
 USE FLIPYEARDB;
 
 
--- PERFIL GERAL CLIENTS e ADM
-INSERT INTO USERS_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  DS_SENHA, DS_TIER)
-						VALUES (1, "carlinhos", "0", 0, "igor@gmail.com", "12345678", "NORMAL_USER");
+-- PERFIL CLIENTES 
+INSERT INTO CLIENTES_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  DS_SENHA, DS_TIER)
+						VALUES (1, "MARK PLIER", "0", 0, "mark@ADMIN.com", "12345678", "ADM");
 							   
 		select *
-			from USERS_TB 			AS U_TB
+			from CLIENTES_TB 			AS U_TB
 			INNER JOIN ENDERECOS_TB AS E_TB 
-									ON E_TB.ID_ENDERECO= U_TB.ID_ENDERECO
-			WHERE DS_TIER = "ADM";
+									ON E_TB.ID_ENDERECO= U_TB.ID_ENDERECO;
+		
+        SELECT *
+			FROM LOGIN_ALL_USERS_TB;
+            
+										
+	
 			
 			SELECT 	ID_CLIENTE,
 					NM_CLIENTE    AS Nome,
@@ -17,7 +22,7 @@ INSERT INTO USERS_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  D
 			FROM    CLIENTES_TB     
 			WHERE   DS_EMAIL      = 'rogerio@gmail.com'
 			AND     DS_SENHA      = '123aa4';
-            
+            -- -------
             SELECT *
                          FROM USERS_TB
                 WHERE NM_USUARIO  LIKE "%rogerio%" 
@@ -26,7 +31,8 @@ INSERT INTO USERS_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  D
 
 		-- ENDERECO
 			INSERT INTO ENDERECOS_TB (DS_CEP, NM_CIDADE, NM_RUA, DS_COMPLEMENTO, NR_NUMERO)
-									  VALUES("341343978", "Beverly hills", " Rua Gigachad da silva "," CASA QUE FICA PERTO DE UMA CALÇADA", 123);
+									  VALUES("0", "adm", "0","0", 0),
+											("341343978", "Beverly hills", " Rua Gigachad da silva "," CASA QUE FICA PERTO DE UMA CALÇADA", 123);
 									 
 		-- PRODUTOS
 			INSERT INTO PRODUTO_TB  (ID_CATEGORIA, NM_PRODUTO, VL_PRECO, VL_PRECO_PROMOCIONA, BT_PRMOCAO, BT_DESTAQUE, BT_DISPONIVEL, QTD_ESTOQUE, DS_DETALHES)
@@ -34,16 +40,17 @@ INSERT INTO USERS_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  D
 									
 			INSERT INTO CATEGORIA_TB (NM_CATEGORIA)
 									 VALUES("Fliperama");
+-- ---------------------
 
-  ----									 
+  -- ----									 
 			SELECT *
 				FROM PRODUTO_TB 		AS P 
 			INNER JOIN CATEGORIA_TB		AS C ON C.ID_CATEGORIA = P.ID_CATEGORIA;
-  ----
-	---	DELETE
+  -- --
+	-- -	DELETE
 		DELETE FROM TB_PRODUTO
-			WHERE ID_PRODUTO = ?
-	---
+			WHERE ID_PRODUTO = ?;
+	-- -
 			UPDATE PRODUTO_TB
 				SET 	NM_PRODUTO					=?,
 						VL_PRECO					=?,
@@ -53,4 +60,4 @@ INSERT INTO USERS_TB (ID_ENDERECO, NM_USUARIO, DS_TELEFONE, DS_CPF, DS_EMAIL,  D
 						BT_DISPONIVEL				=?,
 						QTD_ESTOQUE					=?,
 						DS_DETALHES					=?,
-				WHERE ID_PRODUTO				=?
+				WHERE   ID_PRODUTO				= ?;
