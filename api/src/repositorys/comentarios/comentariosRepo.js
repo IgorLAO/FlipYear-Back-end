d98ab5de4a433c92d1d18f630fdf72b59a0751e1
João Felipe <call.joao.37@gmail.com>
2023-10-18 11:53:30 -0300

    ajustes do comentarioRepo

--- api/src/repositorys/comentarios/comentariosRepo.js
+++ api/src/repositorys/comentarios/comentariosRepo.js
@@ -3,8 +3,8 @@ import config from "../DB/db_connection.js";
 export async function ListComments(qtd, offset){
     const sql = `select US_TB.ID_USUARIO 							AS ID,
     NM_USUARIO 							AS NOME,
-    DS_COMENTARIO 						AS COMENTÁRIO,
-    DT_COMENTARIO						AS PUBLICAÇÃO,
+    DS_COMENTARIO 						AS COMENTARIO,
+    DT_COMENTARIO						AS PUBLICACAO,
     QTD_LIKES							AS LIKES
 FROM COMENTARIOS_TB 						AS C_TB
 INNER JOIN USERS_TB AS US_TB
