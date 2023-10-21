4638570ffe23e1275ec6e5367c56e512468d7ce5
Igor_LAO <igoli6767@gmail.com>
2023-10-21 17:08:15 -0300

    imagem

--- api/src/controllers/carrinhoCtrl.js
+++ api/src/controllers/carrinhoCtrl.js
@@ -4,77 +4,56 @@ import { InserirProdutoNoCarrinho, ListarCarrinho, LimparCarrinho, RemoverProdut
 let server = Router();
 
 server.get('/carrinho/:id', async (req, resp) => {
-
-
     try {
         const { id } = req.params;
-         let data = await ListarCarrinho(id);
-         resp.send(data);
-
+        let data = await ListarCarrinho(id);
+        resp.send(data);
 
     }
-    
-    catch (err){
 
+    catch (err) {
         resp.send(err.message);
 
     }
 });
 
 server.post('/carrinho', async (req, resp) => {
-
     try {
-
         const info = req.body;
 
         const resposta = await InserirProdutoNoCarrinho(info);
         resp.send(resposta);
 
-
     }
 
-    catch(err) {
-
-        resp.status(400).send ({
+    catch (err) {
+        resp.status(400).send({
             erro: err.message
         });
-
     }
-
 });
 
-
 server.delete('/carrinho/:id', async (req, resp) => {
-
-
     try {
         const { id } = req.params;
-         let data = await LimparCarrinho(id);
-         resp.status(204).send();
-
+        let data = await LimparCarrinho(id);
+        resp.status(204).send();
 
     }
-    
-    catch (err){
-
+    catch (err) {
         resp.send(err.message);
 
     }
 });
 
-
 server.delete('/carrinho/produto/:iduser/:idproduto', async (req, resp) => {
-
-
     try {
         const { iduser, idproduto } = req.params;
-         let data = await RemoverProdutoDoCarrinho(iduser, idproduto);
-         resp.status(204).send();
+        let data = await RemoverProdutoDoCarrinho(iduser, idproduto);
+        resp.status(204).send();
 
     }
-    
-    catch (err){
-
+    catch (err) {
         resp.send(err.message);
 
     }
@@ -82,39 +61,29 @@ server.delete('/carrinho/produto/:iduser/:idproduto', async (req, resp) => {
 
 
 server.put('/carrinho/produto/:qtd/:idproduto', async (req, resp) => {
-
-
     try {
         const { qtd, idproduto } = req.params;
-         let data = await AlterarProdutosDoEstoque(qtd, idproduto);
-         resp.status(204).send();
+        let data = await AlterarProdutosDoEstoque(qtd, idproduto);
+        resp.status(204).send();
 
     }
-    
-    catch (err){
-
+    catch (err) {
         resp.send(err.message);
 
     }
 });
 
 server.put('/carrinho/:qtd/:iduser/:idproduto', async (req, resp) => {
-
-
     try {
         const { qtd, idproduto, iduser } = req.params;
-         let data = await AlterarQtdNoCarrinho(qtd, idproduto, iduser);
-         resp.status(204).send();
+        let data = await AlterarQtdNoCarrinho(qtd, idproduto, iduser);
+        resp.status(204).send();
 
     }
-    
-    catch (err){
-
+    catch (err) {
         resp.send(err.message);
 
     }
 });
-
-
 
 export default server;
