9200849a7ac8c659185b08bd799474934d3594ae
Igor_LAO <igoli6767@gmail.com>
2023-11-23 22:32:11 -0300

    a

--- api/src/repositorys/db_connection.js
+++ api/src/repositorys/db_connection.js
@@ -1,19 +1,23 @@
-import mysql from "mysql2/promise";
-
-const config = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PWD,
-    database: process.env.MYSQL_DB,
-    typeCast: function(field, next) {
-        if(field.type === 'TINY' && field.length === 1) {
-            return field.string() === '1';
-        } else {
-            return next();
+try {
+    const config = await mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PWD,
+        database: process.env.MYSQL_DB,
+        typeCast: function(field, next) {
+            if(field.type === 'TINY' && field.length === 1) {
+                return field.string() === '1';
+            } else {
+                return next();
+            }
         }
-    }
-});
-
-console.log('DB!');
-
-export default config
+    });
+    
+    console.log('Conexão bem-sucedida ao banco de dados MySQL!');
+    
+} catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+    throw error;
+}
+export default config;
+  
